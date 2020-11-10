a33f972cd2692e6f0a5c624dbee73ecc3b931a47
kuribayashi1026 <e1b18032@st.oit.ac.jp>
2020-11-10 15:18:33 +0900

    リポジトリの初期化

---
+++ src/main/java/group5/number_hit/NumberHitApplication.java
@@ -0,0 +1,13 @@
+package group5.number_hit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberHitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberHitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group5/number_hit/NumberHitApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.number_hit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberHitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
