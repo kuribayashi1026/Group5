1c40ea7e8e258f0f82289a6c40780f1232e71132
kou026 <e1b18023@st.oit.ac.jp>
2020-12-15 16:31:07 +0900

    hit, hand の値をDBの追加できるように変更

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,13 +7,13 @@ CREATE TABLE yubisuma_room (
     id INT NOT NULL
 );
 CREATE TABLE data (
-    id INT,
-    hp INT,
+    id INT NOT NULL PRIMARY KEY,
+    hp INT NOT NULL,
     hand INT,
     hit INT
 );
 CREATE TABLE talk (
-    id INT,
+    id INT PRIMARY KEY,
     name CHAR,
     talk CHAR
 );
